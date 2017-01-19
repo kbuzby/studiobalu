6069e37c06f270bceddbf568c02d272a2fd3e7ec
Kyle Buzby <kyle.buzby@gmail.com>
2017-01-18 21:12:07 -0600

    add content and product models, product controller including photo uploader

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -1,22 +1,68 @@
 class ProductsController < ApplicationController
+  def index
+    category = params[:category]
+    if category != nil
+      @products = Product.where(category: category)
+    else
+      @products = Product.all
+    end
+  end
+
   def new
+    if !admin?
+      redirect_to products_path
+    end
+
+    @product = Product.new
   end
 
   def create
-  end
+    if !admin?
+      redirect_to products_path
+    end
 
-  def index
+    @product = Product.new(product_params)
+    if @product.save
+      redirect_to edit_product_path(@product)
+    end
   end
 
   def destroy
-  end
+    if !admin?
+      redirect_to product_path(params[:id])
+    end
 
-  def edit
-  end
-
-  def update
+    @product = Product.find(params[:id])
   end
 
   def show
+    @product = Product.find(params[:id])
   end
+
+  def update
+    if !admin?
+      redirect_to product_path(params[:id])
+    end
+
+    @product = Product.find(params[:id])
+    if @product.update_attributes(product_params)
+      redirect_to product_path(@product)
+    else
+      redirect_to edit_product_path(@product)
+    end
+
+  end
+
+  def edit
+    if !admin?
+      redirect_to product_path(params[:id])
+    end
+
+    @product = Product.find(params[:id])
+  end
+
+  private
+    def product_params
+      params.require(:post).permit(:name, :category, :description, :price, :image_path)
+    end
 end
--- app/models/product.rb
+++ app/models/product.rb
@@ -1,2 +1,3 @@
 class Product < ApplicationRecord
+  mount_uploader :image_path, PhotoUploader
 end
