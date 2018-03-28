6506ec34013a38b18f932010ad9f00c64b5bf0da
Kyle Buzby <kyle.buzby@gmail.com>
2018-03-27 20:51:11 -0500

    update deploy config

--- config/deploy.rb
+++ config/deploy.rb
@@ -1,5 +1,5 @@
 # config valid only for current version of Capistrano
-lock '3.4.1'
+lock '3.10.1'
 
 set :application, 'studiobalu'
 set :repo_url, 'git@github.com:kbuzby/studiobalu.git'
@@ -8,7 +8,7 @@ set :repo_url, 'git@github.com:kbuzby/studiobalu.git'
 # ask :branch, `git rev-parse --abbrev-ref HEAD`.chomp
 
 # Default deploy_to directory is /var/www/my_app_name
-set :deploy_to, '/home/ruby-deploy/studiobalu'
+set :deploy_to, '/home/deploy/studiobalu'
 
 # Default value for :scm is :git
 # set :scm, :git
